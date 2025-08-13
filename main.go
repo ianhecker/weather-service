a7930a3dd340e93fc3735ef9b85568e24e7149d9
ian hecker <ianhecker.dev@gmail.com>
2025-08-13 12:08:12 -0500

    Chore[main.go] Pull weather data from API and format forecast into struct

--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"jack.henry/weather-svc/client"
+	"jack.henry/weather-svc/data"
+	"jack.henry/weather-svc/weather"
+)
 
 func main() {
-	fmt.Println("hello world!")
+	client := client.NewClient()
+
+	request, err := client.NewRequest(http.MethodGet, "https://api.weather.gov/gridpoints/TOP/31,80/forecast")
+	checkErr(err)
+
+	response, err := client.Do(request)
+
+	var data data.Data
+	err = data.UnmarshalJSON(response)
+	checkErr(err)
+
+	period, err := data.GetPeriod(0)
+	checkErr(err)
+
+	var weather weather.Weather
+	err = weather.UnmarshalJSON(period)
+	checkErr(err)
+
+	fmt.Printf("%v\n", weather)
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
